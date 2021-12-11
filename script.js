51ca9c23b03e42e361410a0746fddc8661cf7cc0
Yandev051 <yanderfrancisco@gmail.com>
2021-12-10 21:24:53 -0300

    inicio de um sonho

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function adicionar() {
+    var nome = document.getElementById('txtnome').value;
+    var res = document.getElementById('res');
+
+
+    res.innerHTML = `O nome Inserido foi ${nome}`
+
+}
